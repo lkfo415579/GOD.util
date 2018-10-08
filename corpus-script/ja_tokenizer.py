56d15c316a0c3436f883616f34394cd0b3252a1b
lkfo415579 <lkfo415579@hotmail.com>
2018-10-08 04:07:56 -0400

    ja

---
+++ corpus-script/ja_tokenizer.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import codecs
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+import MeCab
+
+sys.stdout = codecs.getwriter('UTF-8')(sys.stdout)
+if sys.argv[1] == '-h':
+    print "python ja_tokenizer.py filename > output"
+
+mecab_ko = MeCab.Tagger("-O wakati".encode("utf-8"))
+# main
+file = sys.argv[1]
+f_ko = codecs.open(file, 'r').readlines()
+
+for line in f_ko:
+    line = line.strip()
+    line = mecab_ko.parse(line.encode("utf-8")).decode('utf8')
+    sys.stdout.write(line)
