4c3795bc42b4a8a197c4afb26977d2eb7c6d3834
lkfo415579 <gm0648@hotmail.com>
2020-03-10 17:47:27 +0800

    plt best

--- performance/plt_best_bleu.py
+++ performance/plt_best_bleu.py
@@ -1,8 +1,13 @@
 import sys
 import yaml
 
+MAX = -1
+BEST = []
 for i in sys.argv[1:]:
-    f = open(i + "/valid.log", 'r').readlines()
+    try:
+        f = open(i + "/valid.log", 'r').readlines()
+    except:
+        continue
     b = []
     for l in f:
         if l.find('translation') != -1:
@@ -14,8 +19,13 @@ for i in sys.argv[1:]:
     # get config
     with open(i + '/model_revo.npz.yml', 'r') as f:
         y = yaml.load(f, Loader=yaml.FullLoader)
-    other = 'maxi-batch: %s' % y['maxi-batch']
-    print(i, b[0], other)
-
+    other = 'maxi-batch: %s, valid: %s' % (y['maxi-batch'], y['valid-sets'])
+    current = i, b[0], other
+    print(current)
+    if b[0][1] > MAX:
+        MAX = b[0][1]
+        BEST = current
+print("=" * 50 + "BEST" + "=" * 50)
+print(BEST) 
 
 
